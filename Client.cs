c31f1e14f0cee19d9a00b1f5301ddf6c851f6b3e
Tariz Shahid <tarizshahid@gmail.com>
2023-09-26 16:07:05 +0500

    Initial commit

---
+++ Client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class ChatClient
+{
+    static void Main()
+    {
+        Console.Write("Enter the server IP address: ");
+        string serverIp = Console.ReadLine();
+
+        Console.Write("Enter the server port: ");
+        int serverPort = int.Parse(Console.ReadLine());
+
+        TcpClient client = new TcpClient(serverIp, serverPort);
+        NetworkStream stream = client.GetStream();
+
+        Console.Write("Enter your name: ");
+        string? name = Console.ReadLine();
+        byte[] nameBytes = Encoding.UTF8.GetBytes(name);
+        stream.Write(nameBytes, 0, nameBytes.Length);
+
+        Console.WriteLine($"Connected to {serverIp}:{serverPort}");
+        Console.WriteLine($"Welcome, {name}!");
+
+        // Start a thread to receive messages from the server
+        var receiveThread = new System.Threading.Thread(() =>
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    Console.WriteLine("Server has disconnected.");
+                    break;
+                }
+
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine(message);
+            }
+        });
+        receiveThread.Start();
+
+        // Send messages to the server
+        while (true)
+        {
+            string message = Console.ReadLine();
+            if (!string.IsNullOrEmpty(message))
+            {
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                stream.Write(messageBytes, 0, messageBytes.Length);
+            }
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class ChatServer
+{
+    private static readonly Dictionary<Socket, string> clients = new Dictionary<Socket, string>();
+    private static readonly object clientLock = new object();
+
+    static void Main()
+    {
+
+        // Listen on any available IP address on port 12345
+        IPAddress ipAddress = IPAddress.Any;
+        int port = 1025;
+        Console.WriteLine($"Chat Server is running... on port :{port}");
+
+        // Create a TCP/IP socket
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the IP address and port
+        IPEndPoint endPoint = new IPEndPoint(ipAddress, port);
+        serverSocket.Bind(endPoint);
+
+        // Start listening for incoming connections
+        serverSocket.Listen(10);
+
+        while (true)
+        {
+            Console.WriteLine("Waiting for a connection...");
+            Socket clientSocket = serverSocket.Accept();
+
+            // Ask for the user's name
+            byte[] nameBuffer = new byte[1024];
+            int receivedBytes = clientSocket.Receive(nameBuffer);
+            string name = Encoding.UTF8.GetString(nameBuffer, 0, receivedBytes);
+
+            lock (clientLock)
+            {
+                clients[clientSocket] = name;
+            }
+
+            string welcomeMessage = $"Welcome, {name}!";
+            byte[] welcomeMessageBuffer = Encoding.UTF8.GetBytes(welcomeMessage);
+            clientSocket.Send(welcomeMessageBuffer);
+
+            // Create a new thread to handle the client
+            Thread clientThread = new Thread(() => HandleClient(clientSocket, name));
+            clientThread.Start();
+        }
+    }
+
+    private static void HandleClient(Socket clientSocket, string name)
+    {
+        try
+        {
+            while (true)
+            {
+                byte[] messageBuffer = new byte[1024];
+                int receivedBytes = clientSocket.Receive(messageBuffer);
+                if (receivedBytes == 0)
+                {
+                    break; // Client has disconnected
+                }
+
+                string message = Encoding.UTF8.GetString(messageBuffer, 0, receivedBytes);
+                string timestamp = DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss] ");
+                string formattedMessage = $"{timestamp}{name}: {message}";
+
+                lock (clientLock)
+                {
+                    foreach (var client in clients.Keys)
+                    {
+                        if (client != clientSocket)
+                        {
+                            client.Send(Encoding.UTF8.GetBytes(formattedMessage));
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception)
+        {
+            // Handle any exceptions (e.g., client disconnects)
+        }
+        finally
+        {
+            lock (clientLock)
+            {
+                clients.Remove(clientSocket);
+            }
+
+            clientSocket.Close();
+            Console.WriteLine($"{name} has left the chat.");
+        }
+    }
+}
